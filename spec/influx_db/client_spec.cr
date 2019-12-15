09d6d877ebedb8e57cddec14b5cfbce3448b73a2
Kim Burgess <kim@acaprojects.com>
2019-12-15 18:48:13 +1000

    test(client): single point writing

--- spec/influx_db/client_spec.cr
+++ spec/influx_db/client_spec.cr
@@ -20,6 +20,22 @@ describe InfluxDB::Client do
             "precision" => "s",
             "org"       => "foo",
           },
+          body: points.first.to_s
+        )
+      client.write "test", points.first
+    end
+
+    it "writes batches of points" do
+      WebMock.stub(:post, "http://example.com/api/v2/write")
+        .with(
+          headers: {
+            "Authorization" => "Token abc",
+          },
+          query: {
+            "bucket"    => "test",
+            "precision" => "s",
+            "org"       => "foo",
+          },
           body: points.join '\n'
         )
       client.write "test", points
