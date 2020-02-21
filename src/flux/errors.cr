c9cdd980b8f8518af224f7d3339c3cae666a637e
Kim Burgess <kim@acaprojects.com>
2020-02-21 14:47:23 +1000

    feat(errors): bubble up flux error messages

--- src/flux/errors.cr
+++ src/flux/errors.cr
@@ -1,14 +1,19 @@
 require "http/client/response"
+require "json"
 
 module Flux
   # Base class for all error types originating from interaction with InfluxDB.
   abstract class Error < Exception
     # Contructs a concrete error object from a client response.
     def self.from(response : HTTP::Client::Response) : Error?
-      message = response.status_message || "HTTP #{response.status_code})"
+      message = nil
+      begin
+        message = JSON.parse(response.body)["message"].as_s
+      rescue
+        message = response.status_message || "HTTP #{response.status_code})"
+      end
+
       case response.status_code
-      when 200..299
-        nil
       when 429
         retry_after = (response.headers["Retry-After"]? || 30).to_i
         TooManyRequests.new message, retry_after
